e048a3716fd6870f3b3a20e1ef1857d8eeefc96b
jordanbuchman <jordan.m.buchman@gmail.com>
2016-05-30 18:33:37 -0600

    Update index.js

--- morph/index.js
+++ morph/index.js
@@ -1,19 +1,7 @@
-/*
-Try GreenSock's MorphSVGPlugin for free.
- 
- - Press the Fork button
- - add your own SVG, JS and HTML
- - have fun.
- 
- Learn more about MorphSVGPlugin here: http://greensock.com/morphSVG
- MorphSVGPlugin is a bonus plugin for Shocking Green and Business Green members: http://www.greensock.com/club
-
-*/
 
 var tl = new TimelineLite({onUpdate:updateSlider}),
-    circle = document.getElementById("circle");
-tl.to(circle, 1, {morphSVG:"#two"}, "+=1")
-  .to(circle, 1, {morphSVG:"#three"}, "+=1")
+    circle = document.getElementById("two");
+tl.to(circle, 1, {morphSVG:"#three"}, "+=1")
 .to(circle, 1, {morphSVG:"#four"}, "+=1")
 .to(circle, 1, {morphSVG:"#five"}, "+=1")
 .to(circle, 1, {morphSVG:"#six"}, "+=1")
